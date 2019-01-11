eb7f7ea6a0e62e8ee48ff4bfdd1771cabd7d00f9
shatinik <rexpower@tut.by>
2019-01-11 11:35:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,112 +1,66 @@
-﻿const express = require('express');
-const crypto = require("crypto");
-const Sequelize = require('sequelize');
+const express = require('express');
+const URL = require('url');
 
 const app = express();
-const port = 80;
-const maxLinkLength = 255; // в БД поле link типа letchar(255)
-const randBytes = 4; // функцией randomBytes получаем байты и преобразуем в hex-строку из 8 символов. в БД стоит тип char(8)
-
-if (!process.env.dblogin) {
-  console.log('process.env.dblogin is not specified');
-  return;
-}
-
-if (!process.env.dbpass) {
-  console.log('process.env.dbpass is not specified');
-  return;
-}
-
-const sequelize = new Sequelize('localhost', process.env.dblogin, process.env.dbpass, {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-app.use(express.urlencoded({ extended: false }))
+const port = 8005;
 
 app.get('/', (req, res) => {
   let tpl = `<!doctype html>
   <html>
     <head>
-      <title> Shortlink page</title>
+      <title>Parser page</title>
     </head>
     <body>
-      <form method="POST">
-        <input type="url" placeholder="input your link here" name="link" maxlength="${maxLinkLength}">
+      <form method="GET" action="/parse">
+        <input type="url" placeholder="input your link here" name="link">
         <input type="submit">
       </form>
     </body>
   </html>`;
-  res.status(200).send(tpl));
-}
-
-
-app.post('/', async (req, res) => {
-  if (!req.body.link) {
+  res.status(200).send(tpl);
+});
+	
+app.get('/parse', (req, res) => {
+  let link = req.query.link;
+  if (!link) {
     res.status('400').send('link is not specified');
     return;
-  } else if (req.body.link.length > maxLinkLength) {
-    res.status('400').send(`link length greater than ${maxLinkLength}`);
+  }
+	
+  let valid = /^(http|https):\/\/[^ "]+$/.test(link);
+  if (!valid) {
+    res.status('400').send('link is not valid');
     return;
   }
-  let link = req.body.link;
-  let valid = /^(ftp|http|https):\/\/[^ "]+$/.test(link);
-  let short = '';
-  let check = true;
-  
-  if (!valid) {
-	res.status('400').send('link is not valid');
-	return;
-  }
-  while (check) {
-    short = crypto.randomBytes(4).toString('hex');
-    matches = await sequelize.query('SELECT `id` FROM `sht_links` WHERE `short`=?', {
-      replacements: [short], 
-      type: sequelize.QueryTypes.SELECT 
-    });
-    if (matches.length == 0) {
-      check = false;
+	
+  let url = URL.parse(link);
+  let message = '';
+  switch (url.host) {
+    case 'cryptorated.com': {
+      let parsed = 4.2;
+      message = `Parsed value: ${parsed}`;
+      break;
     }
+    default: {
+      let host = url.host;
+      message = `Host: ${host} is not supported`;
+      break;
+    }	  
   }
-  await sequelize.query('INSERT INTO `sht_links`(`link`, `short`) VALUES (?, ?)', {
-    replacements: [link, short], 
-    type: sequelize.QueryTypes.INSERT 
-  });
   let tpl = `<!doctype html>
-    <html>
-      <head>
-        <title> Shortlink page</title>
-      </head>
-      <body>
-        Your link to ${link}: <a href="${short}">localhost/${short}</a>
-      </body>
-    </html>`;
-  res.status(201).send(tpl);
+  <html>
+    <head>
+      <title>Parser page</title>
+    </head>
+    <body>
+      ${message}
+    </body>
+  </html>`;
+  res.status(200).send(tpl);
 });
 
 app.get('*', async (req, res) => {
-  let short = req.originalUrl.substr(1);
-  if (short.length != 8 || !(/[0-9a-f]/.test(short))) {
-    res.status('400').send('link is not valid');
-	return;
-  }
-  let matches = await sequelize.query('SELECT `link` FROM `sht_links` WHERE `short`=?', {
-    replacements: [short], 
-    type: sequelize.QueryTypes.SELECT 
-  });
-  if (matches.length == 0) {
-    res.status(404).send();
-	return;
-  }
-  res.redirect(matches[0].link);
+  res.redirect('/');
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-    app.listen(port, () => console.log(`App listening on port ${port}!`));
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+app.listen(port, () => console.log(`App listening on port ${port}!`));
